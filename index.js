c0b486efb683d92399fb759122fa6a362eb183cb
Viswanth005 <viswanths.pillai@gmail.com>
2026-07-11 19:18:20 +0530

    Included keyboard Input

--- index.js
+++ index.js
@@ -16,3 +16,37 @@ function calculate(){
 function clearScreen(){
     display.value="";
 }
+
+
+//Adding keyboard Input
+document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    if ("0123456789".includes(key)) {
+        appendtoDisplay(key);
+    }
+    else if ("+-*/".includes(key)) {
+        appendtoDisplay(key);
+    }
+    else if (key === ".") {
+        appendtoDisplay(".");
+    }
+    else if (key === "Enter" || key === "=") {
+        event.preventDefault();
+        calculate();
+    }
+    else if (key === "Escape") {
+        clearScreen();
+    }
+    else if (key === "Backspace") {
+        display.value = display.value.slice(0, -1);
+    }
+});
+
+//Clear Syntax Error
+function appendtoDisplay(val) {
+    if (display.value === "Syntax Error") {
+        display.value = "";
+    }
+
+    display.value += val;
+}
